d0742a1ff78833d7f8de835c5aebf7c317acb72c
denys <ddatsko010101@gmail.com>
2024-02-12 00:06:10 +0100

    Small changes in visualization script

--- scripts/visualize_paths.py
+++ scripts/visualize_paths.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 import sys
 import yaml
 import math
-from typing import List, Tuple
 from matplotlib.colors import hsv_to_rgb
 
 METERS_IN_DEGREE = 111319.5
 
 
 def gps_coordinates_to_meters(point: (float, float), lat_lon_origin: (float, float)) -> (float, float):
+    """
+    Transform points in gps latitude, longitude into meters relatively to lat_lon_origin
+    :param point: point to transform
+    :param lat_lon_origin: relative point to gent the position from
+    """
     # TODO: This function ignores Earth curvature. This has to be replaced with a better calculation
     meters_in_long_degree = math.cos((lat_lon_origin[0] / 180.0) * math.pi) * METERS_IN_DEGREE
     return point[1] * meters_in_long_degree - lat_lon_origin[1] * meters_in_long_degree, point[0] * METERS_IN_DEGREE - \
@@ -17,6 +21,10 @@ def gps_coordinates_to_meters(point: (float, float), lat_lon_origin: (float, flo
 
 
 def read_csv(filename: str, lat_lon_origin: (float, float) or None) -> np.array:
+    """
+    Read csv file into np.array transforming latitude,longitude into meters if needed
+    :param lat_lon_origin: (latitude, longitude) origin if points are in (latitude, longitude), None if points are already in meters
+    """
     res = np.loadtxt(filename, delimiter=",", dtype=np.float64)
     if lat_lon_origin is not None:
         res = np.apply_along_axis(lambda p: gps_coordinates_to_meters(p, lat_lon_origin), 1, res)
@@ -37,11 +45,13 @@ def main():
             print(e)
             exit(-1)
 
+    # Set the origin for transformation to lat_lon origin from config file if one exists
     if configuration["points_in_lat_lon"]:
         lat_lon_origin = configuration["latitude_origin"], configuration["longitude_origin"]
     else:
         lat_lon_origin = None
 
+    # Read all the needed files
     fly_zone_points = read_csv(configuration["fly_zone_filename"], lat_lon_origin)
     no_fly_zones_points = []
     paths_points = []
@@ -52,6 +62,7 @@ def main():
     for path_filename in sys.argv[2:]:
         paths_points.append(read_csv(path_filename, lat_lon_origin))
 
+    # Plot the AOI and paths
     plt.figure()
 
     fly_zone_points = np.vstack([fly_zone_points, fly_zone_points[0]])
@@ -61,11 +72,15 @@ def main():
         no_fly_zone = np.vstack([no_fly_zone, no_fly_zone[0]])
         plt.plot(no_fly_zone[:, 0], no_fly_zone[:, 1], color='red')
 
-    colors = [hsv_to_rgb([i / len(paths_points), 0.9, 0.9]) for i in range(len(paths_points))]
     for i in range(len(paths_points)):
-        plt.plot(paths_points[i][:, 0], paths_points[i][:, 1], color=colors[i])
+        # Plot each path using a distinct color
+        plt.plot(paths_points[i][:, 0], paths_points[i][:, 1],
+                 color=hsv_to_rgb([0.2 + 0.8 * (i / len(paths_points)), 0.8, 0.8]))
 
-
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Visualization of AOI and generated paths")
+    plt.axis('equal')
 
     plt.show()
 
